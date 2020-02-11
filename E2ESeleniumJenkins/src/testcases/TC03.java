c9d6a69a8bf47784737dd5853656ed21387d1249
aksha <aksha@192.168.1.140>
2020-02-10 19:23:35 -0600

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC03.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC03 {
+
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Tester");
+		driver.findElement(By.id("pass")).sendKeys("123123a");
+		driver.quit();
+			
+	}
+}
